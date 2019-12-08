ee08e74992babeba04c56929678849f170d1a5fb
Ocraz <registered@quicknet.nl>
2019-12-08 21:22:57 +0100

    Add files via upload

--- getsld.py
+++ getsld.py
@@ -3,60 +3,60 @@
 
 import sys
 import re
+import signal
+
+def signal_handler(sig, frame):
+        print("\nCtrl-C detected, exiting...\n")
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 aInFqdn = []
 
 fileDoubleTld = "doubletld.txt"
 fileSingleTld = "singletld.txt"
 
-
 def removeTld(sFqdn, fileTld):
-    #aFqdnCapped = []
-    global aInFqdn
     
     f = open(fileTld, 'r')
     aTld = f.readlines()
     f.close()
-
+    
+    aFqdnToDotmp = []
+    
     for sTld in aTld:
         sTld = sTld.strip()
-        
-        strTld.replace(".", "\.")
-        if sFqdn.endswith(sTld):
-            sFqdnCapped = re.sub(r'\.'+sTld+"$", '', sFqdn)
-            aInFqdn.remove(sFqdn)
-            
-        #if sFqdn != sFqdnCapped:
-            #aFqdnCapped.append(sFqdnCapped)
-      #  if aInFqdn.count(sFqdn) > 0:
-            #print ("removing " + line2 + " from " + sFqdn)
-            #print ( aInFqdn.count(sFqdn))
-            
-       #     break
-            print ( sFqdnCapped)
-        #else:
-         #   print ("not removing: " + ">"+sFqdn+"<" + ">"+sFqdnCapped+"<" + line2 + "<")
-    #print (sFqdn)                
-    #print (sFqdnCapped)                
-    
+                
+        if sFqdn.endswith("." + sTld):
+            sFqdnCapped = re.sub(r'\.' + sTld + "$", '', sFqdn)
+            aFqdnToDotmp.append(sFqdn)
+    return aFqdnToDotmp
+
 
 for sInFqdn in sys.stdin:
     aInFqdn.append(sInFqdn.strip())
 
-print ("Double TLDs stripped")
+print ("Double TLDs stripped:")
+aFqdnToDo = []
 for sInFqdn in aInFqdn:
-    sInFqdnCapped = removeTld(sInFqdn, fileDoubleTld)
-    #sys.stdout.write (sInFqdnCapped+"\n")
-    #for sStrippedFqdn in aInFqdn:
-        #sys.stdout.write (sStrippedFqdn + "\n")
+    aFqdnToDo = removeTld(sInFqdn, fileDoubleTld)
 
+    #for sFqdnToDo in aFqdnToDo:
+     #   aInFqdn.remove(sFqdnToDo)
+
+    for sFqdnToDo in aFqdnToDo:
+        print (sFqdnToDo)
+    #aInFqdn.remove(sFqdnToDo)
+
+        
+#sys.exit()
 print ("")
-print ("Single TLDs stripped")
+print ("Single TLDs stripped:")
 for sInFqdn in aInFqdn:
-    sInFqdnCapped = removeTld(sInFqdn, fileSingleTld)
-    #sys.stdout.write (sInFqdnCapped+"\n")
-    #for ssippedFqdn in aInFqdn:
-     #   sys.stdout.write (ssippedFqdn + "\n")
+    aFqdnToDo = removeTld(sInFqdn, fileSingleTld)
+    for sFqdnToDo in aFqdnToDo:
+        print (sFqdnToDo)
+        #aInFqdn.remove(sFqdnToDo)
 
 sys.stdout.write ("\n")
     
